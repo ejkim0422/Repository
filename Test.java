74d228570acd6fbd4fbd14f4610aaa0d6833957e
ejkim0422 <ejkim0422@gmail.com>
2014-03-26 23:19:00 +0900

    delete comment

--- Test.java
+++ Test.java
@@ -13,23 +13,10 @@ public class Test {
 	
 	public void add(){
 		Test01 t1 = new Test01();
-
-		// 김은주는 이쁘다. 그러나 너무 이쁘다.
-		// 강광구는 요즘 디아블로에 빠졌다.
-		// 그만 해야한다..
-		// 그러나 계속한다.
-
-		/* add joyana
-		   강광구는 아주 멋있다.
-		   김은주는 너무 이쁘다.
-		*/
-
 		Test01.test01_sub();
 	}
 	
 	public static void sub(){
-	//add sub branch 
-	// add s sub_branch
 		
 	}
 
@@ -37,8 +24,7 @@ public class Test {
 
 class Test01 {
 	public void test01_add() {
-		// add master_branch
-		// add s master_branch
+		
 	}
 
 	public static void test01_sub() {
