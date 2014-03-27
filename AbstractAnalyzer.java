b7010b01e364544aa0e5479347685224c49d2202
ejkim0422 <ejkim0422@gmail.com>
2014-03-27 16:24:17 +0900

    add file

---
+++ AbstractAnalyzer.java
@@ -0,0 +1,12 @@
+package com.codescroll.qscroll.revisionCreator;
+
+public abstract class AbstractAnalyzer {
+	protected int t_revisionId;
+	
+	public abstract void analyze();
+}
+
+// indicator
+public void indicator(){
+
+}
